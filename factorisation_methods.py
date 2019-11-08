0e248264413a184accee6816cb40aa187aa1f851
Vincent Wong <TheBrutux168@gmail.com>
2019-11-08 20:31:59 +1100

    Add fermat factorisation method

--- factorisation_methods.py
+++ factorisation_methods.py
@@ -24,6 +24,20 @@ def naive_trial_factorisation(n: int) -> List[int]:
 
     return factors
 
+def fermat_factorisation(n: int) -> (int, int):
+    '''
+    Tests all t from sqrt(n) to n.
+    If s^2 = t^2 - n is a square, then return n = ab = (t + s)(t - s).
+    '''
+
+    for t in range(math.ceil(math.sqrt(n)), n + 1):
+        s_squared = t ** 2 - n
+        s = math.sqrt(s_squared)
+
+        if int(s) == math.sqrt(s_squared):
+            return [int(s + t), int(t - s)]
+
 
 if __name__ == "__main__":
-    print(naive_trial_factorisation(97))
+    print(naive_trial_factorisation(12412312312412351))
+    print(fermat_factorisation(231412312321))
