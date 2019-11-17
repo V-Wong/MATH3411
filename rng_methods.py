f051006b15a289505e58be4e5760608e420848ab
Vincent Wong <TheBrutux168@gmail.com>
2019-11-17 15:05:27 +1100

    Add intermediate steps to middle square rng method

--- rng_methods.py
+++ rng_methods.py
@@ -2,18 +2,28 @@ import math
 
 
 def middle_squares(n: int, seed: int) -> int:
+    '''
+    Iterate:
+        x_i+1 = (x_i)^2
+        Add leading 0's so that x_i+1 has 2n digits
+        Crop x_i+1 to middle n digits
+    '''
+
     for _ in range(n):
         seed = seed ** 2
-
+    
         while len(str(seed)) < 2 * n:
             seed = "0" + str(seed)
 
-        while len(str(seed)) != n:
-            seed = str(seed)[1:-1]
+        cropped_seed = seed
+        while len(str(cropped_seed)) != n:
+            cropped_seed = str(cropped_seed)[1:-1]
 
-        seed = int(seed)
+        print(f"{seed} -> {cropped_seed}")
+
+        seed = int(cropped_seed)
 
     return seed
 
 if __name__ == "__main__":
-    print(middle_squares(4, 2100))
+    print("Final seed: ", middle_squares(4, 2100))
