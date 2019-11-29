d6d96f4e83f9c091f8d72ad7b0f6ac61024a38c2
Vincent Wong <TheBrutux168@gmail.com>
2019-11-29 16:41:43 +1100

    Add large modulo function

--- number_theory.py
+++ number_theory.py
@@ -10,5 +10,15 @@ def gcd(a: int, b: int) -> int:
     return gcd(b, a % b)
 
 
+def modulo(base: int, pow: int, mod: int) -> int:
+    res = 1
+
+    for _ in range(1, pow + 1):
+        res *= base
+        res %= mod
+
+    return res
+
+
 if __name__ == "__main__":
     print(gcd(32, 6))
